e44df372ee6f96b69af06f4593de2164dd95c101
netbuilderbd <50077833+netbuilderbd@users.noreply.github.com>
2019-07-16 16:23:30 +0600

    Add files via upload

---
+++ js/agency.js
@@ -0,0 +1,70 @@
+// Agency Theme JavaScript
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function(){ 
+            $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+})(jQuery); // End of use strict
+
+
+// Go to Top
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 500) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
+});
+$(document).ready(function(){
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 500) {
+			$('.scrollSocial').fadeIn();
+		} else {
+			$('.scrollSocial').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	// $('.scrollSocial').click(function(){
+	// 	$('html, body').animate({scrollTop : 0},800);
+	// 	return false;
+	// });
+	
+});
